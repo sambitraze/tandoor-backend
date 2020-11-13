5f61078e095c1667ee2f6f93a2888eebc939a91f
sambitraze <majhisambit2@gmail.com>
2020-11-13 23:51:29 +0530

    nodejs express mongo added

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const temproute = require("./routes/temp");
 const itemroute = require("./routes/item");
 const orderRoute = require("./routes/order");
 const pdfRoute = require("./routes/pdfmaker");
+const path = require("path");
 const cors = require("cors");
+const http = require("http");
 require("dotenv/config");
 
 app.use(cors());
 app.use(bodyparser.json());
+app.use("/admin", express.static(path.join(__dirname, '/dashboard')));
 app.use("/api", temproute);
 app.use("/item", itemroute);
 app.use("/order", orderRoute);
@@ -32,5 +35,11 @@ app.get("/", (req, res) => {
   res.send("server is live");
 });
 
-//
-app.listen(3000);
+// for local
+// app.listen(3000);
+
+//for server 
+const httpServer = http.createServer(app);
+httpServer.listen(80, () => {
+  console.log("HTTP Server running on port 80");
+});
