68f06c21115246b605495ee5a1e58a3d22b02029
SAMBIT MAJHI <majhisambit2@gmail.com>
2020-12-02 22:19:29 +0530

    dsad

--- models/delivery.js
+++ models/delivery.js
@@ -1,52 +1,56 @@
 const mongoose = require("mongoose");
 var deliverySchema = mongoose.Schema(
-    {
-      name: {
-        type: String,
-        maxlength: 64,
-        required: true,
-        trim: true,
-      },
-      email: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true,
-      },
-      phone: {
-        type: String,
-        trim: true,
-      },
-      latitude: {
-        type: String,
-        trim: true,
-        default: "0.0"
-      },
-      longitude: {
-        type: String,
-        trim: true,
-        default: "0.0"
-      },
-      deviceToken: {
-        type: String,
-        trim: true,
-      },
-      password: {
-        type: String,
-        trim: true,
-      },            
-      assigned: {
-        type: String,
-        trim: true,
-        default: "0"
-      },
-      completed: {
-        type: String,
-        trim: true,
-        default: "0"
-      },
+  {
+    name: {
+      type: String,
+      maxlength: 64,
+      required: true,
+      trim: true,
     },
-    { timestamps: true }
-  );
-  
-  module.exports = mongoose.model("Delivery", deliverySchema, "DeliveryBoy");
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    phone: {
+      type: String,
+      trim: true,
+    },
+    latitude: {
+      type: String,
+      trim: true,
+      default: "0.0",
+    },
+    longitude: {
+      type: String,
+      trim: true,
+      default: "0.0",
+    },
+    deviceToken: {
+      type: String,
+      trim: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+    },
+    blocked: {
+      type: Boolean,
+      default: false,
+    },
+    assigned: {
+      type: String,
+      trim: true,
+      default: "0",
+    },
+    completed: {
+      type: String,
+      trim: true,
+      default: "0",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Delivery", deliverySchema, "DeliveryBoy");
