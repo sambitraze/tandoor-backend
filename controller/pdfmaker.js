cdbc91fa23fff6753304ecac480669eb6f8e571c
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-01-26 00:38:19 +0530

    Update pdfmaker.js

--- controller/pdfmaker.js
+++ controller/pdfmaker.js
@@ -46,7 +46,7 @@ exports.createPdf = (req, res) => {
         parseFloat(req.body.gst)
       ).toFixed(2);
     '</td></tr><tr><td></td><td></td><td></td><td colspan="2">Offer</td><td>' +
-      "-"(
+      "-"+(
         parseFloat(req.body.amount) *
           0.01 *
           parseFloat(req.body.offer.percentage)
